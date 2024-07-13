32d5ecb2ce13567d67adc48872bc6b2e902da738
Piyachai2310 <119798286+Piyachai2310@users.noreply.github.com>
2024-07-13 14:29:42 +0700

    Update_UI

---
+++ src/Auth_Login.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Auth_Login = () => {
+    return(
+        <div>
+            <Outlet/>
+        </div>
+
+    )
+}
+
+export default Auth_Login;
